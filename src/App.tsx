59fe3148624277d3568b3f442352a81efbca6bfd
Adam Doyle <adamldoyle@gmail.com>
2021-02-17 02:35:38 -0600

    Basic math flash cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Card from './components/Card';
+
+interface IEquation {
+  firstNumber: number;
+  secondNumber: number;
+  operator: '+' | '-' | 'x' | '/';
+}
+
+const operators = ['+', '-', 'x'];
+
+const generateRandomEquation = (): IEquation => {
+  const operator = operators[
+    Math.floor(Math.random() * operators.length)
+  ] as any;
+  const bigNumber = operator === '+' || operator === '-';
+  const number1 = Math.floor(Math.random() * (bigNumber ? 100 : 10));
+  const number2 = Math.floor(Math.random() * (bigNumber ? 100 : 10));
+  return {
+    firstNumber: number1 > number2 ? number1 : number2,
+    secondNumber: number1 > number2 ? number2 : number1,
+    operator: operator,
+  };
+};
 
 function App() {
+  const [equation, setEquation] = useState<IEquation>(generateRandomEquation());
+  const [total, setTotal] = useState<number>(1);
+  const [totalRight, setTotalRight] = useState<number>(0);
+  const [streak, setStreak] = useState<number>(0);
+
+  const onAnswer = (correct: boolean) => {
+    setTotalRight(correct ? totalRight + 1 : totalRight);
+    setStreak(correct ? streak + 1 : 0);
+  };
+
+  const onNextQuestion = () => {
+    setTotal(total + 1);
+    setEquation(generateRandomEquation());
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Card
+        key={total}
+        firstNumber={equation.firstNumber}
+        secondNumber={equation.secondNumber}
+        operator={equation.operator}
+        onAnswer={onAnswer}
+        onNextQuestion={onNextQuestion}
+      />
+      <div style={{ fontSize: '2rem', color: '#000077' }}>Streak: {streak}</div>
+      <div style={{ fontSize: '2rem', color: '#000077' }}>
+        Total: {totalRight} out of {total}
+      </div>
     </div>
   );
 }
---
+++ src/components/Card/index.tsx
@@ -0,0 +1,146 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+
+const operatorFunctions: {
+  [operator: string]: (a: number, b: number) => number;
+} = {
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  x: (a, b) => a * b,
+  '/': (a, b) => a / b,
+};
+
+export default function Card({
+  firstNumber,
+  secondNumber,
+  operator,
+  onAnswer,
+  onNextQuestion,
+}: {
+  firstNumber: number;
+  secondNumber: number;
+  operator: '+' | '-' | 'x' | '/';
+  onAnswer: (correct: boolean) => void;
+  onNextQuestion: () => void;
+}) {
+  const answer = operatorFunctions[operator](firstNumber, secondNumber);
+  const [submittedAnswer, setSubmittedAnswer] = useState<number | ''>('');
+  const [correct, setCorrect] = useState<boolean | null>(null);
+
+  const changeAnswer = (evt: ChangeEvent<HTMLInputElement>) => {
+    const value = evt.target.value;
+    setSubmittedAnswer(value ? parseInt(value) : '');
+  };
+
+  const onSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    setCorrect(answer === submittedAnswer);
+    onAnswer(answer === submittedAnswer);
+  };
+
+  const nextQuestion = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    onNextQuestion();
+  };
+
+  if (correct === true || correct === false) {
+    return (
+      <div
+        style={{
+          width: '70vw',
+          height: '70vh',
+          border: '10px solid #000077',
+          borderRadius: 10,
+          backgroundColor: correct ? '#007700' : '#770000',
+          textAlign: 'center',
+          color: '#ffffff',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-evenly',
+          alignItems: 'center',
+          fontSize: '10rem',
+        }}
+      >
+        <div>
+          {!correct && (
+            <span
+              style={{ textDecoration: 'line-through', marginRight: '5rem' }}
+            >
+              {submittedAnswer}
+            </span>
+          )}
+          {answer}
+        </div>
+        <form onSubmit={nextQuestion}>
+          <button
+            type="submit"
+            autoFocus
+            style={{
+              fontSize: '7rem',
+              backgroundColor: 'inherit',
+              border: 'none',
+              color: '#ffffff',
+            }}
+          >
+            Next <ArrowForwardIosIcon style={{ fontSize: '4rem' }} />
+          </button>
+        </form>
+      </div>
+    );
+  }
+
+  return (
+    <div
+      style={{
+        width: '70vw',
+        height: '70vh',
+        border: '10px solid #000077',
+        borderRadius: 10,
+        textAlign: 'center',
+        color: '#000077',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
+      }}
+    >
+      <div style={{ fontSize: '8rem' }}>
+        {firstNumber} {operator} {secondNumber}
+      </div>{' '}
+      <div style={{ fontSize: '6rem' }}>=</div>
+      <form
+        onSubmit={onSubmit}
+        style={{ display: 'flex', justifyContent: 'space-evenly' }}
+      >
+        <input
+          type="number"
+          placeholder="?"
+          value={submittedAnswer}
+          onChange={changeAnswer}
+          autoFocus
+          style={{
+            fontSize: '8rem',
+            width: '60%',
+            textAlign: 'center',
+            color: '#000077',
+            border: '5px solid #000077',
+            borderRadius: 10,
+          }}
+        />
+        <button
+          type="submit"
+          style={{
+            color: '#ffffff',
+            backgroundColor: '#000077',
+            borderColor: '#000077',
+            border: '5px solid #000077',
+            borderRadius: 10,
+            height: '100%',
+          }}
+          disabled={submittedAnswer === ''}
+        >
+          <ArrowForwardIosIcon style={{ fontSize: '6rem' }} />
+        </button>
+      </form>
+    </div>
+  );
+}
